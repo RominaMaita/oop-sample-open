cbdb12329c6edd9299d1bc92a3528729aa0bb32e
Ro <u202213765@upc.edu.pe>
2025-04-02 10:50:11 -0500

    feat(Money): add money value object.

---
+++ src/main/java/com/acme/oop/shared/domain/model/valueobjects/Money.java
@@ -0,0 +1,34 @@
+package com.acme.oop.shared.domain.model.valueobjects;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+public record Money(BigDecimal amount, Currency currency) {
+    public Money{
+        if (amount == null ) {
+            throw new IllegalArgumentException("Amount cannot be null");
+        }
+        if (currency == null ) {
+            throw new IllegalArgumentException("Currency cannot be null");
+        }
+        if(amount.scale() > 2){
+            throw new IllegalArgumentException("Amount cannot be greater than 2");
+        }
+    }
+
+    public static Money zero(){
+        return new Money(BigDecimal.ZERO, Currency.getInstance("USD"));
+    }
+
+    public Money addMoney(Money other){
+        if(!this.currency.equals(other.currency)){
+            throw new IllegalArgumentException("Cannot add moneys with different currency");
+        }
+        return new Money(amount.add(other.amount), this.currency);
+    }
+
+    public Money multiply(int multiplier){
+        return new Money(this.amount.multiply(BigDecimal.valueOf(multiplier)), this.currency);
+    }
+
+}
